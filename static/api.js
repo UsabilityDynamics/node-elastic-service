d4b6907e779be679e74be5f5125bd3ca8d90a677
Andy Potanin <andy.potanin@usabilitydynamics.com>
2013-07-05 17:55:46 -0400

    Automatic update.

---
+++ static/api.js
@@ -0,0 +1,36 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Index",
+        "Type"
+    ],
+    "modules": [
+        "ElasticClient",
+        "Index",
+        "Type",
+        "elastic-client"
+    ],
+    "allModules": [
+        {
+            "displayName": "elastic-client",
+            "name": "elastic-client",
+            "description": "Elastic Client Utility"
+        },
+        {
+            "displayName": "ElasticClient",
+            "name": "ElasticClient",
+            "description": "Elastic Client"
+        },
+        {
+            "displayName": "Index",
+            "name": "Index",
+            "description": "Index Module\n\n-"
+        },
+        {
+            "displayName": "Type",
+            "name": "Type",
+            "description": "Type Module\n\n-"
+        }
+    ]
+} };
+});
---
+++ test/api.js
@@ -0,0 +1,31 @@
+/**
+ * Mocha Test for api
+ *
+ * mocha api --reporter list --ui exports --watch
+ *
+ * @author potanin@UD
+ * @date 7/5/13
+ * @type {Object}
+ */
+module.exports = {
+
+  /**
+   * Prepare Environment
+   *
+   */
+  'before': function() {},
+
+  'Elastic Client': {
+
+    // Very basic test
+    'is available.': function() {
+      var EC = require( '../' );
+      EC.should.have.property( 'utility' );
+      EC.should.have.property( 'logger' );
+      EC.should.have.property( 'start' );
+      EC.should.have.property( 'defaults' );
+    }
+
+  }
+
+};
